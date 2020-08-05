e33a048ab7d4c7eb845420a34d3e6d32bcdfc72e
Duskfen <Gabriel@waldviertelblick.at>
2020-08-05 23:34:44 +0200

    setting up project, added main menu scene, main menu button signals, created theme

---
+++ Project/Scenes/Menu/MenuBtns.gd
@@ -0,0 +1,26 @@
+extends Control
+
+onready var PlayBtn = get_node("Play")
+onready var CustomBtn = get_node("Custom")
+onready var SettingsBtn = get_node("Settings")
+onready var HowToBtn = get_node("How to")
+
+
+# -- Main Menu Buttons
+func _on_Play_pressed():
+	print("Play Button pressed")
+	#TODO 
+
+func _on_Custom_pressed():
+	print("Custom Button pressed")
+	#TODO
+	
+func _on_Settings_pressed():
+	print("Settings Button pressed")
+	#TODO
+	
+func _on_How_to_pressed():
+	print("How to Button pressed")
+	#TODO
+
+# -- Main Menu Buttons
