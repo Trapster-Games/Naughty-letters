07a9e62ba6e914b2ad802d7cdd5b44e2a02e0a6c
Duskfen <Gabriel@waldviertelblick.at>
2020-08-12 16:59:16 +0200

    edited game start behaviour

---
+++ Project/Scenes/StartScene/startscene.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	get_node("AnimationPlayer").play("logo")
+	
+	SceneChanger.change_scene("res://Scenes/Menu/Menu.tscn", 2)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
